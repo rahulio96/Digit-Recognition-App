8f154128bcf82b023c7c931b00a0f7340c932035
rahulio96 <rahulgithub96@gmail.com>
2023-08-27 13:41:00 -0700

    Added neural network integration

--- flask-server/server.py
+++ flask-server/server.py
@@ -6,51 +6,60 @@ from flask_cors import CORS
 from functions import *
 import numpy as np
 import pandas as pd
+from PIL import Image
 import base64
+import io
 
 app = Flask(__name__)
-#CORS(app)
+CORS(app)
 
-@app.route('/api/receive', methods=['POST'])
-def api_receive_image():
-    data = request.get_json()
-    image_data = data.get('image')
-    return jsonify({'message': 'Image received!!!'})
+@app.route('/a', methods=['GET', 'POST'])
+def receive_image():
+    if (request.method == 'POST'):
+        data = request.get_json()
 
+        data = data.get('image')
 
+        # Decode base64 image data
+        image_data = base64.b64decode(data.split(',')[1])
+        
+        image = Image.open(io.BytesIO(image_data))
+
+        # Greyscale image
+        image = image.convert("L")
+
+        # Convert the image to a NumPy array
+        image_array = np.array(image)
+
+        # Flatten the image array to a 1D array
+        flattened_array = image_array.reshape(-1)  # shape: (784,)
+
+        # Normalize pixel values to [0, 1]
+        normalized_array = flattened_array / 255
+
+        #new_img = image_array.reshape((image_array.shape[0]*image_array.shape[1]), 1)
+        arr = normalized_array.reshape(1, -1).T
+
+        # Convert the data to numpy arrays
+        w1 = np.array(pd.read_csv('./weights-biases/trained_weights/w1.csv'))
+        w2 = np.array(pd.read_csv('./weights-biases/trained_weights/w2.csv'))
+        b1 = np.array(pd.read_csv('./weights-biases/trained_biases/b1.csv'))
+        b2 = np.array(pd.read_csv('./weights-biases/trained_biases/b2.csv'))
+
+        # Test neural network
+        x_test = arr
+
+        # Perform forward propagation on the test data
+        weight_sum1, weight_sum2, active_output1, active_output2 = forward_propagation(w1, w2, b1, b2, x_test)
+
+        # Make predictions
+        predictions = np.argmax(active_output2)  # Get the index of the maximum probability for each test sample
+
+        # Print the predictions
+        print( np.argmax(active_output2, axis=0) )
+        return jsonify({'message': str(predictions)})
+
+    return jsonify({'message': 'POST request not given'})
     
-    """# Load the weights from CSV files 
-    w1_data = pd.read_csv('./weights-biases/trained_weights/w1.csv')
-    w2_data = pd.read_csv('./weights-biases/trained_weights/w2.csv')
-    b1_data = pd.read_csv('./weights-biases/trained_biases/b1.csv')
-    b2_data = pd.read_csv('./weights-biases/trained_biases/b2.csv')
-
-    # Convert the data to numpy arrays
-    w1 = np.array(w1_data)
-    w2 = np.array(w2_data)
-    b1 = np.array(b1_data)
-    b2 = np.array(b2_data)
-
-    # Test neural network
-
-    # Load and preprocess the test data
-    test_data = pd.read_csv("./data/test.csv")  # TODO: NEEEEEEEEED TOOOOOOOOOO REPLAAAAAAAAAAAAAACE!!!!!!!!!!!!!
-    test_data = np.array(test_data).T
-    x_test = test_data[:, 0, None]
-
-    # Perform forward propagation on the test data
-    weight_sum1, weight_sum2, active_output1, active_output2 = forward_propagation(w1, w2, b1, b2, x_test)
-
-    # Make predictions
-    predictions = np.argmax(active_output2, axis=0)  # Get the index of the maximum probability for each test sample
-
-    # Print the predictions
-    print(predictions)"""
-
-
-"""@app.route("/api/message")
-def api_message():
-    return jsonify(message="Hello from Flask!")
-"""
 if __name__ == "__main__":
-    app.run(host="localhost")
+    app.run(host="localhost", debug=True)
