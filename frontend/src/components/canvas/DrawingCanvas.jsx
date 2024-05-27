c2e69ad7005694907f0910d29078d0895ecce9e0
rahulio96 <rahgupta96@gmail.com>
2024-05-27 10:05:46 -0700

    Remove access-control-allow-origin

--- frontend/src/components/canvas/DrawingCanvas.jsx
+++ frontend/src/components/canvas/DrawingCanvas.jsx
@@ -89,7 +89,6 @@ const DrawingCanvas = ({updatePrediction}) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "https://drawdigits.netlify.app/",
       },
       body: JSON.stringify({image: dataURL}),
     })
